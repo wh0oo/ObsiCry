ac46f27b010d4914ba43f433403abd0b79799993
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-05-23 16:55:22 -0500

    Create ObsiCryInitializer.java

---
+++ src/main/java/com/example/obsicry/ObsiCryInitializer.java
@@ -0,0 +1,8 @@
+package com.example.obsicry;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ObsiCryInitializer implements ModInitializer {
+    @Override
+    public void onInitialize() {}
+}
