6c6b8400f6121bc6f0aae8c247bd05b11289a8a2
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-05-24 20:24:49 -0500

    Update PortalShapeMixin.java

--- src/main/java/com/example/mixin/PortalShapeMixin.java
+++ src/main/java/com/example/mixin/PortalShapeMixin.java
@@ -1,23 +1,18 @@
 package com.example.mixin;
 
-import net.minecraft.block.Block;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.world.dimension.NetherPortal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "net.minecraft.class_7420")
+@Mixin(NetherPortal.class)
 public abstract class PortalShapeMixin {
 
-    @Redirect(
-        method = "method_48210", // placePortal
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
-        )
-    )
-    private boolean obsicry$allowCryingObsidian(BlockState state, Block block) {
-        return state.isOf(Blocks.OBSIDIAN) || state.isOf(Blocks.CRYING_OBSIDIAN);
+    @ModifyReturnValue(method = "isValidFrameBlock", at = @At("RETURN"))
+    private static boolean obsicry$allowCryingObsidian(boolean original, @Local(argsOnly = true) BlockState state) {
+        return original || state.isOf(Blocks.CRYING_OBSIDIAN);
     }
 }
