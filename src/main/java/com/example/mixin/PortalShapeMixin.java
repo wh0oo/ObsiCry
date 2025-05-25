31a69a5d1356c3384ba47d6fb2712a487600cfbe
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-05-24 19:56:52 -0500

    Update PortalShapeMixin.java

--- src/main/java/com/example/mixin/PortalShapeMixin.java
+++ src/main/java/com/example/mixin/PortalShapeMixin.java
@@ -3,16 +3,15 @@ package com.example.mixin;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.world.portal.PortalShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(PortalShape.class)
+@Mixin(targets = "net.minecraft.class_5518")
 public abstract class PortalShapeMixin {
 
     @Redirect(
-        method = "isValidFrameBlock",
+        method = "method_30485",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z"
